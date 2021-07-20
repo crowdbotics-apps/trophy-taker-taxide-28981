98dc471e48bfd62848593d040e33b14584a5c584
Crowdbotics <team@crowdbotics.com>
2021-07-20 16:55:50 +0000

    Initial commit.

---
+++ backend/trophy_taker_taxide_28981/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trophy_taker_taxide_28981 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trophy_taker_taxide_28981.settings')
+
+application = get_wsgi_application()
